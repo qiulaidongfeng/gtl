d791e43dcd8598c9315849bd1c7bb21c7247a8a6
秋来冬风 <2645477756@qq.com>
2022-01-01 21:53:56 +0800

    gunsafe

---
+++ gunsafe/gunsafe.go
@@ -0,0 +1,10 @@
+// unsafe
+package gunsafe
+
+import "unsafe"
+
+func Stob(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
