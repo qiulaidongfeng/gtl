8041691ba580f722e776bcffe81ba482c4a79d47
秋来冬风 <2645477756@qq.com>
2022-01-09 22:02:41 +0800

    sstack

--- stack/sstack.go
+++ stack/sstack.go
@@ -8,82 +8,84 @@ import (
 )
 
 const (
-	Int8size       uintptr = (unsafe.Sizeof(int8(1)))
-	Int16size      uintptr = (unsafe.Sizeof(int16(1)))
-	Int32size      uintptr = (unsafe.Sizeof(int32(1)))
-	Int64size      uintptr = (unsafe.Sizeof(int64(1)))
-	Intsize        uintptr = (unsafe.Sizeof(int(1)))
-	Uint8size      uintptr = (unsafe.Sizeof(uint8(1)))
-	Uint16size     uintptr = (unsafe.Sizeof(uint16(1)))
-	Uint32size     uintptr = (unsafe.Sizeof(uint32(1)))
-	Uint64size     uintptr = (unsafe.Sizeof(uint64(1)))
-	Uintsize       uintptr = (unsafe.Sizeof(uint(1)))
-	Bytesize       uintptr = (unsafe.Sizeof(byte(1)))
-	Runesize       uintptr = (unsafe.Sizeof(rune(1)))
-	Boolsize       uintptr = (unsafe.Sizeof(bool(true)))
-	Float32size    uintptr = (unsafe.Sizeof(float32(1.0)))
-	Float64size    uintptr = (unsafe.Sizeof(float64(2.0)))
-	Uintptrsize    uintptr = (unsafe.Sizeof(uintptr(7)))
-	Complex64size  uintptr = (unsafe.Sizeof(complex64(6 + 9i)))
-	Complex128size uintptr = (unsafe.Sizeof(complex128(8 + 9i)))
-	Interfacesize  uintptr = (unsafe.Sizeof(*(new(interface{}))))
+	Int8size       uint64 = uint64((unsafe.Sizeof(int8(1))))
+	Int16size      uint64 = uint64((unsafe.Sizeof(int16(1))))
+	Int32size      uint64 = uint64((unsafe.Sizeof(int32(1))))
+	Int64size      uint64 = uint64((unsafe.Sizeof(int64(1))))
+	Intsize        uint64 = uint64((unsafe.Sizeof(int(1))))
+	Uint8size      uint64 = uint64((unsafe.Sizeof(uint8(1))))
+	Uint16size     uint64 = uint64((unsafe.Sizeof(uint16(1))))
+	Uint32size     uint64 = uint64((unsafe.Sizeof(uint32(1))))
+	Uint64size     uint64 = uint64((unsafe.Sizeof(uint64(1))))
+	Uintsize       uint64 = uint64((unsafe.Sizeof(uint(1))))
+	Bytesize       uint64 = uint64((unsafe.Sizeof(byte(1))))
+	Runesize       uint64 = uint64((unsafe.Sizeof(rune(1))))
+	Boolsize       uint64 = uint64((unsafe.Sizeof(bool(true))))
+	Float32size    uint64 = uint64((unsafe.Sizeof(float32(1.0))))
+	Float64size    uint64 = uint64((unsafe.Sizeof(float64(2.0))))
+	Uintptrsize    uint64 = uint64((unsafe.Sizeof(uintptr(7))))
+	Complex64size  uint64 = uint64((unsafe.Sizeof(complex64(6 + 9i))))
+	Complex128size uint64 = uint64((unsafe.Sizeof(complex128(8 + 9i))))
+	Interfacesize  uint64 = uint64((unsafe.Sizeof(*(new(interface{})))))
 )
 
 type GenericitySavememoryStack struct {
-	slice []int8
-	size  *uint64
-	scap  *uint64
-	mutex sync.RWMutex
+	slice  []int8
+	size   *uint64
+	scap   *uint64
+	bmutex bool
+	mutex  sync.RWMutex
 }
 
 type Sstack = GenericitySavememoryStack
 
 func NewGsstack() Sstack {
 	s := Sstack{
-		slice: make([]int8, 2, 2),
-		size:  new(uint64),
-		scap:  new(uint64),
+		slice:  make([]int8, 2, 2),
+		size:   new(uint64),
+		scap:   new(uint64),
+		bmutex: false,
 	}
-	s.scap = 2
+	*s.scap = 2
 	return s
 }
 
 func (s *Sstack) addcap() (ncap uint64) {
-	s.slice = append(s.slice, 9)
-	ncap = cap(s.slice)
+	s.slice = append(s.slice, 2)
+	ncap = uint64(cap(s.slice))
 	nslice := make([]int8, ncap, ncap)
-	for i := 0; i < (ncap - 1); i++ {
+	for i := uint64(0); i < (ncap - 1); i++ {
 		nslice[i] = s.slice[i]
 	}
+	s.slice = nslice
 	return
 }
 
 func (s *Sstack) Tsaddcap() (ncap uint64) {
 	s.mutex.Lock()
 	s.slice = append(s.slice, 9)
-	ncap = cap(s.slice)
+	ncap = uint64(cap(s.slice))
 	nslice := make([]int8, ncap, ncap)
-	for i := 0; i < (ncap - 1); i++ {
+	for i := uint64(0); i < (ncap - 1); i++ {
 		nslice[i] = s.slice[i]
 	}
-	s.mutex.Ulock()
+	s.mutex.Unlock()
 	return
 }
 
 func (s *Sstack) Pushint8(x int8) error {
-	if (*s.scap) == (*s.size) {
+	if (*s.size)+Int8size >= (*s.scap) {
 		*s.scap = s.addcap()
 	}
-	s.slice[size] = x
-	s.size++
+	s.slice[*s.size] = x
+	*s.size++
 	return nil
 }
 
-func (s *Sstack) TsPushint8(x int8) {
+func (s *Sstack) TsPushint8(x int8) error {
 	s.mutex.RLock()
 	nsize := atomic.AddUint64(s.size, Int8size)
-	ok := atomic.CompareAndSwapUint64(s.scap, nsize-Int8size, ((*s.scap) + 100))
-	if ok {
+	if nsize >= *s.scap {
 		s.mutex.RUnlock()
 		*s.scap = s.Tsaddcap()
 		s.mutex.RLock()
@@ -94,8 +96,8 @@ func (s *Sstack) TsPushint8(x int8) {
 	return nil
 }
 
-func (s *Sstack) Pushint16(x int16) {
-	if (*s.scap) == (*s.size) {
+func (s *Sstack) Pushint16(x int16) error {
+	if (*s.size)+Int16size >= (*s.scap) {
 		*s.scap = s.addcap()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
@@ -103,8 +105,8 @@ func (s *Sstack) Pushint16(x int16) {
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int16)(sp)
 	*sp2 = x
-	s.size += Int16size
-	return
+	*s.size += Int16size
+	return nil
 }
 
 func (s *Sstack) Pushint32(x int32) {
