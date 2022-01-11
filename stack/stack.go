c2dd9929f755504903553d8fb2332afecdf72cbb
秋来冬风 <2645477756@qq.com>
2022-01-11 12:03:03 +0800

    stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,10 +1,21 @@
 package stack
 
+import (
+	"errors"
+)
+
+var (
+	StackEmpty        error = errors.New("Stack,Empty")
+	StackSizeExceeded error = errors.New("Stack size exceeded")
+	StackPushFail     error = errors.New("Stack push fail")
+	StackClearFail    error = errors.New("Stack clear fail")
+)
+
 type TsStack interface {
 	Tspush(x interface{}) error
 	Tspop() (interface{}, error)
 	Tssize() uint64
-	Tsclear()
+	Tsclear() error
 	Tslook(size uint64) (interface{}, error)
 }
 
@@ -12,7 +23,7 @@ type NoTsStack interface {
 	Push(x interface{}) error
 	Pop() (interface{}, error)
 	Size() uint64
-	Clear()
+	Clear() error
 	Look(size uint64) (interface{}, error)
 }
 
