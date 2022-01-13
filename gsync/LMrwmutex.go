33a3e7422260fc18859b208f1e8729508cebf1e4
秋来冬风 <2645477756@qq.com>
2022-01-13 14:42:38 +0800

    LMrwmutex

--- gsync/LMrwmutex.go
+++ gsync/LMrwmutex.go
@@ -2,8 +2,14 @@
 package gsync
 
 import (
+	"errors"
 	"runtime"
 	"sync/atomic"
+)
+
+var (
+	Noreadlock  error = errors.New("No read lock")
+	Nowritelock error = errors.New("No write lock")
 )
 
 type LMrwmutex struct {
@@ -11,17 +17,54 @@ type LMrwmutex struct {
 }
 
 func (m *LMrwmutex) Lock() {
-	ok := false
-	for ok == false {
-		ok = atomic.CompareAndSwapInt64(&m.nm, 0, -1)
+	for {
+		ok := atomic.CompareAndSwapInt64(&m.nm, 0, -1)
+		if ok == true {
+			break
+		}
 		runtime.Gosched()
 	}
 }
 
 func (m *LMrwmutex) Unlock() {
-	ok := false
-	for ok == false {
-		ok = atomic.CompareAndSwapInt64(&m.nm, -1, 0)
+	nm := atomic.LoadInt64(&m.nm)
+	if nm >= 0 {
+		panic(Nowritelock)
+	}
+	for {
+		ok := atomic.CompareAndSwapInt64(&m.nm, -1, 0)
+		if ok == true {
+			break
+		}
 		runtime.Gosched()
+	}
+}
+
+func (m *LMrwmutex) RLock() {
+	for {
+		ok := atomic.CompareAndSwapInt64(&m.nm, -1, -1)
+		if ok == true {
+			runtime.Gosched()
+			continue
+		}
+		nm := atomic.AddInt64(&m.nm, 1)
+		if nm == 0 {
+			break
+		}
+	}
+}
+
+func (m *LMrwmutex) RUnlock() {
+	ok := atomic.CompareAndSwapInt64(&m.nm, -1, -1)
+	if ok == true {
+		panic(Noreadlock)
+	}
+	ok = atomic.CompareAndSwapInt64(&m.nm, 0, 0)
+	if ok == true {
+		panic(Noreadlock)
+	}
+	for {
+		atomic.AddInt64(&m.nm, -1)
+		break
 	}
 }
