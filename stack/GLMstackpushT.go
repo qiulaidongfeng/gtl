1b50ba9a7af90fb7137913de974186720334aef3
qiulaidongfeng <2645477756@qq.com>
2022-01-13 11:44:00 +0800

    GLMstack

--- stack/GLMstackpushT.go
+++ stack/GLMstackpushT.go
@@ -6,28 +6,28 @@ import (
 )
 
 func (s *GLMstack) Pushint(x int) error {
-	if (*s.size)+Intsize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Intsize)
+	if s.size+Intsize >= s.scap {
+		s.scap = s.addcap(s.size + Intsize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int)(sp)
 	*sp2 = x
-	*s.size += Intsize
+	s.size += Intsize
 	return nil
 }
 
 func (s *GLMstack) TsPushint(x int) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Intsize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Intsize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Intsize)
+		s.scap = s.Tsaddcap(s.size + Intsize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int)(sp)
 	*sp2 = x
@@ -36,20 +36,20 @@ func (s *GLMstack) TsPushint(x int) error {
 }
 
 func (s *GLMstack) Pushint8(x int8) error {
-	if (*s.size)+Int8size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Int8size)
+	if s.size+Int8size >= s.scap {
+		s.scap = s.addcap(s.size + Int8size)
 	}
-	s.slice[*s.size] = x
-	*s.size++
+	s.slice[s.size] = x
+	s.size++
 	return nil
 }
 
 func (s *GLMstack) TsPushint8(x int8) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Int8size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Int8size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Int8size)
+		s.scap = s.Tsaddcap(s.size + Int8size)
 		s.mutex.RLock()
 	}
 	s.slice[nsize] = x
@@ -58,28 +58,28 @@ func (s *GLMstack) TsPushint8(x int8) error {
 }
 
 func (s *GLMstack) Pushint16(x int16) error {
-	if (*s.size)+Int16size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Int16size)
+	if s.size+Int16size >= s.scap {
+		s.scap = s.addcap(s.size + Int16size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int16)(sp)
 	*sp2 = x
-	*s.size += Int16size
+	s.size += Int16size
 	return nil
 }
 
 func (s *GLMstack) TsPushint16(x int16) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Int16size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Int16size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Int16size)
+		s.scap = s.Tsaddcap(s.size + Int16size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int16)(sp)
 	*sp2 = x
@@ -88,28 +88,28 @@ func (s *GLMstack) TsPushint16(x int16) error {
 }
 
 func (s *GLMstack) Pushint32(x int32) error {
-	if (*s.size)+Int32size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Int32size)
+	if s.size+Int32size >= s.scap {
+		s.scap = s.addcap(s.size + Int32size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int32)(sp)
 	*sp2 = x
-	*s.size += Int32size
+	s.size += Int32size
 	return nil
 }
 
 func (s *GLMstack) TsPushint32(x int32) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Int32size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Int32size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Int32size)
+		s.scap = s.Tsaddcap(s.size + Int32size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int32)(sp)
 	*sp2 = x
@@ -118,28 +118,28 @@ func (s *GLMstack) TsPushint32(x int32) error {
 }
 
 func (s *GLMstack) Pushint64(x int64) error {
-	if (*s.size)+Int64size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Int64size)
+	if s.size+Int64size >= s.scap {
+		s.scap = s.addcap(s.size + Int64size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int64)(sp)
 	*sp2 = x
-	*s.size += Int64size
+	s.size += Int64size
 	return nil
 }
 
 func (s *GLMstack) TsPushint64(x int64) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Int64size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Int64size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Int64size)
+		s.scap = s.Tsaddcap(s.size + Int64size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int64)(sp)
 	*sp2 = x
@@ -148,28 +148,28 @@ func (s *GLMstack) TsPushint64(x int64) error {
 }
 
 func (s *GLMstack) Pushuint(x uint) error {
-	if (*s.size)+Uintsize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uintsize)
+	if s.size+Uintsize >= s.scap {
+		s.scap = s.addcap(s.size + Uintsize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint)(sp)
 	*sp2 = x
-	*s.size += Uintsize
+	s.size += Uintsize
 	return nil
 }
 
 func (s *GLMstack) TsPushuint(x uint) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uintsize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uintsize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uintsize)
+		s.scap = s.Tsaddcap(s.size + Uintsize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint)(sp)
 	*sp2 = x
@@ -178,28 +178,28 @@ func (s *GLMstack) TsPushuint(x uint) error {
 }
 
 func (s *GLMstack) Pushuint8(x uint8) error {
-	if (*s.size)+Uint8size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uint8size)
+	if s.size+Uint8size >= s.scap {
+		s.scap = s.addcap(s.size + Uint8size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint8)(sp)
 	*sp2 = x
-	*s.size += Uint8size
+	s.size += Uint8size
 	return nil
 }
 
 func (s *GLMstack) TsPushuint8(x uint8) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uint8size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uint8size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uint8size)
+		s.scap = s.Tsaddcap(s.size + Uint8size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint8)(sp)
 	*sp2 = x
@@ -208,28 +208,28 @@ func (s *GLMstack) TsPushuint8(x uint8) error {
 }
 
 func (s *GLMstack) Pushuint16(x uint16) error {
-	if (*s.size)+Uint16size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uint16size)
+	if s.size+Uint16size >= s.scap {
+		s.scap = s.addcap(s.size + Uint16size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint16)(sp)
 	*sp2 = x
-	*s.size += Uint16size
+	s.size += Uint16size
 	return nil
 }
 
 func (s *GLMstack) TsPushuint16(x uint16) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uint16size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uint16size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uint16size)
+		s.scap = s.Tsaddcap(s.size + Uint16size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint16)(sp)
 	*sp2 = x
@@ -238,28 +238,28 @@ func (s *GLMstack) TsPushuint16(x uint16) error {
 }
 
 func (s *GLMstack) Pushuint32(x uint32) error {
-	if (*s.size)+Uint32size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uint32size)
+	if s.size+Uint32size >= s.scap {
+		s.scap = s.addcap(s.size + Uint32size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint32)(sp)
 	*sp2 = x
-	*s.size += Uint32size
+	s.size += Uint32size
 	return nil
 }
 
 func (s *GLMstack) TsPushuint32(x uint32) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uint32size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uint32size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uint32size)
+		s.scap = s.Tsaddcap(s.size + Uint32size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint32)(sp)
 	*sp2 = x
@@ -268,28 +268,28 @@ func (s *GLMstack) TsPushuint32(x uint32) error {
 }
 
 func (s *GLMstack) Pushuint64(x uint64) error {
-	if (*s.size)+Uint64size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uint64size)
+	if s.size+Uint64size >= s.scap {
+		s.scap = s.addcap(s.size + Uint64size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint64)(sp)
 	*sp2 = x
-	*s.size += Uint64size
+	s.size += Uint64size
 	return nil
 }
 
 func (s *GLMstack) TsPushuint64(x uint64) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uint64size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uint64size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uint64size)
+		s.scap = s.Tsaddcap(s.size + Uint64size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint64)(sp)
 	*sp2 = x
@@ -298,28 +298,28 @@ func (s *GLMstack) TsPushuint64(x uint64) error {
 }
 
 func (s *GLMstack) PushByte(x byte) error {
-	if (*s.size)+Bytesize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Bytesize)
+	if s.size+Bytesize >= s.scap {
+		s.scap = s.addcap(s.size + Bytesize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*byte)(sp)
 	*sp2 = x
-	*s.size += Bytesize
+	s.size += Bytesize
 	return nil
 }
 
 func (s *GLMstack) TsPushByte(x byte) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Bytesize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Bytesize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Bytesize)
+		s.scap = s.Tsaddcap(s.size + Bytesize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*byte)(sp)
 	*sp2 = x
@@ -328,28 +328,28 @@ func (s *GLMstack) TsPushByte(x byte) error {
 }
 
 func (s *GLMstack) PushRune(x rune) error {
-	if (*s.size)+Runesize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Runesize)
+	if s.size+Runesize >= s.scap {
+		s.scap = s.addcap(s.size) + Runesize
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*rune)(sp)
 	*sp2 = x
-	*s.size += Runesize
+	s.size += Runesize
 	return nil
 }
 
 func (s *GLMstack) TsPushRune(x rune) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Runesize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Runesize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Runesize)
+		s.scap = s.Tsaddcap(s.size + Runesize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*rune)(sp)
 	*sp2 = x
@@ -358,28 +358,28 @@ func (s *GLMstack) TsPushRune(x rune) error {
 }
 
 func (s *GLMstack) PushBool(x bool) error {
-	if (*s.size)+Boolsize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Boolsize)
+	if s.size+Boolsize >= s.scap {
+		s.scap = s.addcap(s.size + Boolsize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*bool)(sp)
 	*sp2 = x
-	*s.size += Runesize
+	s.size += Runesize
 	return nil
 }
 
 func (s *GLMstack) TsPushBool(x bool) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Boolsize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Boolsize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Boolsize)
+		s.scap = s.Tsaddcap(s.size + Boolsize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*bool)(sp)
 	*sp2 = x
@@ -388,28 +388,28 @@ func (s *GLMstack) TsPushBool(x bool) error {
 }
 
 func (s *GLMstack) PushFloat32(x float32) error {
-	if (*s.size)+Float32size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Float32size)
+	if s.size+Float32size >= s.scap {
+		s.scap = s.addcap(s.size + Float32size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float32)(sp)
 	*sp2 = x
-	*s.size += Float32size
+	s.size += Float32size
 	return nil
 }
 
 func (s *GLMstack) TsPushFloat32(x float32) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Float32size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Float32size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Float32size)
+		s.scap = s.Tsaddcap(s.size + Float32size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float32)(sp)
 	*sp2 = x
@@ -418,28 +418,28 @@ func (s *GLMstack) TsPushFloat32(x float32) error {
 }
 
 func (s *GLMstack) PushFloat64(x float64) error {
-	if (*s.size)+Float64size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Float64size)
+	if s.size+Float64size >= s.scap {
+		s.scap = s.addcap(s.size + Float64size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float64)(sp)
 	*sp2 = x
-	*s.size += Float64size
+	s.size += Float64size
 	return nil
 }
 
 func (s *GLMstack) TsPushFloat64(x float64) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Float64size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Float64size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Float64size)
+		s.scap = s.Tsaddcap(s.size + Float64size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float64)(sp)
 	*sp2 = x
@@ -448,28 +448,28 @@ func (s *GLMstack) TsPushFloat64(x float64) error {
 }
 
 func (s *GLMstack) PushUintptr(x uintptr) error {
-	if (*s.size)+Uintptrsize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Uintptrsize)
+	if s.size+Uintptrsize >= s.scap {
+		s.scap = s.addcap(s.size + Uintptrsize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uintptr)(sp)
 	*sp2 = x
-	*s.size += Uintptrsize
+	s.size += Uintptrsize
 	return nil
 }
 
 func (s *GLMstack) TsPushUintptr(x uintptr) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Uintptrsize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Uintptrsize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Uintptrsize)
+		s.scap = s.Tsaddcap(s.size + Uintptrsize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uintptr)(sp)
 	*sp2 = x
@@ -478,28 +478,28 @@ func (s *GLMstack) TsPushUintptr(x uintptr) error {
 }
 
 func (s *GLMstack) PushComplex128(x complex128) error {
-	if (*s.size)+Complex128size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Complex128size)
+	if s.size+Complex128size >= s.scap {
+		s.scap = s.addcap(s.size + Complex128size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex128)(sp)
 	*sp2 = x
-	*s.size += Complex128size
+	s.size += Complex128size
 	return nil
 }
 
 func (s *GLMstack) TsPushComplex128(x complex128) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Complex128size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Complex128size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Complex128size)
+		s.scap = s.Tsaddcap(s.size + Complex128size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex128)(sp)
 	*sp2 = x
@@ -508,28 +508,28 @@ func (s *GLMstack) TsPushComplex128(x complex128) error {
 }
 
 func (s *GLMstack) PushComplex64(x complex64) error {
-	if (*s.size)+Complex64size >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Complex64size)
+	if s.size+Complex64size >= s.scap {
+		s.scap = s.addcap(s.size + Complex64size)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex64)(sp)
 	*sp2 = x
-	*s.size += Complex64size
+	s.size += Complex64size
 	return nil
 }
 
 func (s *GLMstack) TsPushComplex64(x complex64) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Complex64size)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Complex64size)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Complex64size)
+		s.scap = s.Tsaddcap(s.size + Complex64size)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex64)(sp)
 	*sp2 = x
@@ -538,28 +538,28 @@ func (s *GLMstack) TsPushComplex64(x complex64) error {
 }
 
 func (s *GLMstack) PushInterface(x interface{}) error {
-	if (*s.size)+Interfacesize >= (*s.scap) {
-		*s.scap = s.addcap((*s.size) + Interfacesize)
+	if s.size+Interfacesize >= s.scap {
+		s.scap = s.addcap(s.size + Interfacesize)
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*interface{})(sp)
 	*sp2 = x
-	*s.size += Interfacesize
+	s.size += Interfacesize
 	return nil
 }
 
 func (s *GLMstack) TsPushInterface(x interface{}) error {
 	s.mutex.RLock()
-	nsize := atomic.AddUint64(s.size, Interfacesize)
-	if nsize >= *s.scap {
+	nsize := atomic.AddUint64(&s.size, Interfacesize)
+	if nsize >= s.scap {
 		s.mutex.RUnlock()
-		*s.scap = s.Tsaddcap((*s.size) + Interfacesize)
+		s.scap = s.Tsaddcap(s.size + Interfacesize)
 		s.mutex.RLock()
 	}
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := (*s.size)
+	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*interface{})(sp)
 	*sp2 = x
