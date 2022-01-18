6afe4bf84a55a071467e17903c7f085bfc79b2a6
秋来冬风 <2645477756@qq.com>
2022-01-18 15:00:58 +0800

    memory

--- cextend/C/memory.h
+++ cextend/C/memory.h
@@ -2,5 +2,5 @@
 #define MEMORY
 void* Malloc(size_t size) ;
 void Free(void* ptr) ;
-void Memcpy(void *dest, void *src, size_t n) ;
+void Memcpy(uintptr_t dest, uintptr_t src, size_t n) ;
 #endif // MEMORY
