46ffd2843316899332c9335d535564e3ceecbf44
秋来冬风 <2645477756@qq.com>
2022-01-18 15:00:46 +0800

    memory

--- cextend/C/memory.c
+++ cextend/C/memory.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 void* Malloc(size_t size)
 {
@@ -11,7 +12,7 @@ void Free(void *ptr)
     return free(ptr) ;
 }
 
-void Memcpy(void *dest, void *stc, size_t n)
+void Memcpy(uintptr_t dest, uintptr_t stc, size_t n)
 {
     memcpy(dest,stc,n) ;
     return ;
