8ce4cd01d72b946705a8d14bc3f68902c214e668
秋来冬风 <2645477756@qq.com>
2022-01-31 19:56:29 +0800

    GLMstack

--- stack/GLMstackpushT.go
+++ stack/GLMstackpushT.go
@@ -1,14 +1,11 @@
 package stack
 
 import (
-	"sync/atomic"
 	"unsafe"
 )
 
 func (s *GLMstack) Pushint(x int) error {
-	if s.size+Intsize >= s.scap {
-		s.scap = s.addcap(s.size + Intsize)
-	}
+	s.pushsafetycheck(Intsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -20,15 +17,9 @@ func (s *GLMstack) Pushint(x int) error {
 
 func (s *GLMstack) TsPushint(x int) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Intsize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Intsize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                     //入栈记录
+	sl := s.tspushsafetycheck(Intsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int)(sp)
 	*sp2 = x
@@ -38,9 +29,7 @@ func (s *GLMstack) TsPushint(x int) error {
 }
 
 func (s *GLMstack) Pushint8(x int8) error {
-	if s.size+Int8size >= s.scap {
-		s.scap = s.addcap(s.size + Int8size)
-	}
+	s.pushsafetycheck(Int8size) //入栈安全检查
 	s.slice[s.size] = x
 	s.size++
 	return nil
@@ -48,13 +37,8 @@ func (s *GLMstack) Pushint8(x int8) error {
 
 func (s *GLMstack) TsPushint8(x int8) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Int8size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Int8size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                         //入栈记录
+	nsize := s.tspushsafetycheck(Int8size) //入栈安全检查
 	s.slice[nsize] = x
 	s.pushok() //结束记录
 	s.mutex.RUnlock()
@@ -62,9 +46,7 @@ func (s *GLMstack) TsPushint8(x int8) error {
 }
 
 func (s *GLMstack) Pushint16(x int16) error {
-	if s.size+Int16size >= s.scap {
-		s.scap = s.addcap(s.size + Int16size)
-	}
+	s.pushsafetycheck(Int16size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -76,15 +58,9 @@ func (s *GLMstack) Pushint16(x int16) error {
 
 func (s *GLMstack) TsPushint16(x int16) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Int16size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Int16size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                       //入栈记录
+	sl := s.tspushsafetycheck(Int16size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int16)(sp)
 	*sp2 = x
@@ -94,9 +70,7 @@ func (s *GLMstack) TsPushint16(x int16) error {
 }
 
 func (s *GLMstack) Pushint32(x int32) error {
-	if s.size+Int32size >= s.scap {
-		s.scap = s.addcap(s.size + Int32size)
-	}
+	s.pushsafetycheck(Int32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -108,15 +82,9 @@ func (s *GLMstack) Pushint32(x int32) error {
 
 func (s *GLMstack) TsPushint32(x int32) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Int32size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Int32size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                       //入栈记录
+	sl := s.tspushsafetycheck(Int32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int32)(sp)
 	*sp2 = x
@@ -126,9 +94,7 @@ func (s *GLMstack) TsPushint32(x int32) error {
 }
 
 func (s *GLMstack) Pushint64(x int64) error {
-	if s.size+Int64size >= s.scap {
-		s.scap = s.addcap(s.size + Int64size)
-	}
+	s.pushsafetycheck(Int64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -140,15 +106,9 @@ func (s *GLMstack) Pushint64(x int64) error {
 
 func (s *GLMstack) TsPushint64(x int64) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Int64size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Int64size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                       //入栈记录
+	sl := s.tspushsafetycheck(Int64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*int64)(sp)
 	*sp2 = x
@@ -158,9 +118,7 @@ func (s *GLMstack) TsPushint64(x int64) error {
 }
 
 func (s *GLMstack) Pushuint(x uint) error {
-	if s.size+Uintsize >= s.scap {
-		s.scap = s.addcap(s.size + Uintsize)
-	}
+	s.pushsafetycheck(Uintsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -172,15 +130,9 @@ func (s *GLMstack) Pushuint(x uint) error {
 
 func (s *GLMstack) TsPushuint(x uint) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uintsize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uintsize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                      //入栈记录
+	sl := s.tspushsafetycheck(Uintsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint)(sp)
 	*sp2 = x
@@ -190,9 +142,7 @@ func (s *GLMstack) TsPushuint(x uint) error {
 }
 
 func (s *GLMstack) Pushuint8(x uint8) error {
-	if s.size+Uint8size >= s.scap {
-		s.scap = s.addcap(s.size + Uint8size)
-	}
+	s.pushsafetycheck(Uint8size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -204,15 +154,9 @@ func (s *GLMstack) Pushuint8(x uint8) error {
 
 func (s *GLMstack) TsPushuint8(x uint8) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uint8size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uint8size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                       //入栈记录
+	sl := s.tspushsafetycheck(Uint8size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint8)(sp)
 	*sp2 = x
@@ -222,9 +166,7 @@ func (s *GLMstack) TsPushuint8(x uint8) error {
 }
 
 func (s *GLMstack) Pushuint16(x uint16) error {
-	if s.size+Uint16size >= s.scap {
-		s.scap = s.addcap(s.size + Uint16size)
-	}
+	s.pushsafetycheck(Uint16size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -236,15 +178,9 @@ func (s *GLMstack) Pushuint16(x uint16) error {
 
 func (s *GLMstack) TsPushuint16(x uint16) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uint16size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uint16size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                        //入栈记录
+	sl := s.tspushsafetycheck(Uint16size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint16)(sp)
 	*sp2 = x
@@ -254,9 +190,7 @@ func (s *GLMstack) TsPushuint16(x uint16) error {
 }
 
 func (s *GLMstack) Pushuint32(x uint32) error {
-	if s.size+Uint32size >= s.scap {
-		s.scap = s.addcap(s.size + Uint32size)
-	}
+	s.pushsafetycheck(Uint32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -268,15 +202,9 @@ func (s *GLMstack) Pushuint32(x uint32) error {
 
 func (s *GLMstack) TsPushuint32(x uint32) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uint32size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uint32size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                        //入栈记录
+	sl := s.tspushsafetycheck(Uint32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint32)(sp)
 	*sp2 = x
@@ -286,9 +214,7 @@ func (s *GLMstack) TsPushuint32(x uint32) error {
 }
 
 func (s *GLMstack) Pushuint64(x uint64) error {
-	if s.size+Uint64size >= s.scap {
-		s.scap = s.addcap(s.size + Uint64size)
-	}
+	s.pushsafetycheck(Uint64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -300,15 +226,9 @@ func (s *GLMstack) Pushuint64(x uint64) error {
 
 func (s *GLMstack) TsPushuint64(x uint64) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uint64size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uint64size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                        //入栈记录
+	sl := s.tspushsafetycheck(Uint64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uint64)(sp)
 	*sp2 = x
@@ -318,9 +238,7 @@ func (s *GLMstack) TsPushuint64(x uint64) error {
 }
 
 func (s *GLMstack) PushByte(x byte) error {
-	if s.size+Bytesize >= s.scap {
-		s.scap = s.addcap(s.size + Bytesize)
-	}
+	s.pushsafetycheck(Bytesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -332,15 +250,9 @@ func (s *GLMstack) PushByte(x byte) error {
 
 func (s *GLMstack) TsPushByte(x byte) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Bytesize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Bytesize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                      //入栈记录
+	sl := s.tspushsafetycheck(Bytesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*byte)(sp)
 	*sp2 = x
@@ -350,9 +262,7 @@ func (s *GLMstack) TsPushByte(x byte) error {
 }
 
 func (s *GLMstack) PushRune(x rune) error {
-	if s.size+Runesize >= s.scap {
-		s.scap = s.addcap(s.size) + Runesize
-	}
+	s.pushsafetycheck(Runesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -364,15 +274,9 @@ func (s *GLMstack) PushRune(x rune) error {
 
 func (s *GLMstack) TsPushRune(x rune) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Runesize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Runesize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                      //入栈记录
+	sl := s.tspushsafetycheck(Runesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*rune)(sp)
 	*sp2 = x
@@ -382,9 +286,7 @@ func (s *GLMstack) TsPushRune(x rune) error {
 }
 
 func (s *GLMstack) PushBool(x bool) error {
-	if s.size+Boolsize >= s.scap {
-		s.scap = s.addcap(s.size + Boolsize)
-	}
+	s.pushsafetycheck(Boolsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -396,15 +298,9 @@ func (s *GLMstack) PushBool(x bool) error {
 
 func (s *GLMstack) TsPushBool(x bool) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Boolsize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Boolsize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                      //入栈记录
+	sl := s.tspushsafetycheck(Boolsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*bool)(sp)
 	*sp2 = x
@@ -414,9 +310,7 @@ func (s *GLMstack) TsPushBool(x bool) error {
 }
 
 func (s *GLMstack) PushFloat32(x float32) error {
-	if s.size+Float32size >= s.scap {
-		s.scap = s.addcap(s.size + Float32size)
-	}
+	s.pushsafetycheck(Float32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -428,15 +322,9 @@ func (s *GLMstack) PushFloat32(x float32) error {
 
 func (s *GLMstack) TsPushFloat32(x float32) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Float32size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Float32size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                         //入栈记录
+	sl := s.tspushsafetycheck(Float32size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float32)(sp)
 	*sp2 = x
@@ -446,9 +334,7 @@ func (s *GLMstack) TsPushFloat32(x float32) error {
 }
 
 func (s *GLMstack) PushFloat64(x float64) error {
-	if s.size+Float64size >= s.scap {
-		s.scap = s.addcap(s.size + Float64size)
-	}
+	s.pushsafetycheck(Float64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -460,15 +346,9 @@ func (s *GLMstack) PushFloat64(x float64) error {
 
 func (s *GLMstack) TsPushFloat64(x float64) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Float64size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Float64size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                         //入栈记录
+	sl := s.tspushsafetycheck(Float64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*float64)(sp)
 	*sp2 = x
@@ -478,9 +358,7 @@ func (s *GLMstack) TsPushFloat64(x float64) error {
 }
 
 func (s *GLMstack) PushUintptr(x uintptr) error {
-	if s.size+Uintptrsize >= s.scap {
-		s.scap = s.addcap(s.size + Uintptrsize)
-	}
+	s.pushsafetycheck(Uintptrsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -492,15 +370,9 @@ func (s *GLMstack) PushUintptr(x uintptr) error {
 
 func (s *GLMstack) TsPushUintptr(x uintptr) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Uintptrsize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Uintptrsize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                         //入栈记录
+	sl := s.tspushsafetycheck(Uintptrsize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*uintptr)(sp)
 	*sp2 = x
@@ -510,9 +382,7 @@ func (s *GLMstack) TsPushUintptr(x uintptr) error {
 }
 
 func (s *GLMstack) PushComplex128(x complex128) error {
-	if s.size+Complex128size >= s.scap {
-		s.scap = s.addcap(s.size + Complex128size)
-	}
+	s.pushsafetycheck(Complex128size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -524,15 +394,9 @@ func (s *GLMstack) PushComplex128(x complex128) error {
 
 func (s *GLMstack) TsPushComplex128(x complex128) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Complex128size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Complex128size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                            //入栈记录
+	sl := s.tspushsafetycheck(Complex128size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex128)(sp)
 	*sp2 = x
@@ -542,9 +406,7 @@ func (s *GLMstack) TsPushComplex128(x complex128) error {
 }
 
 func (s *GLMstack) PushComplex64(x complex64) error {
-	if s.size+Complex64size >= s.scap {
-		s.scap = s.addcap(s.size + Complex64size)
-	}
+	s.pushsafetycheck(Complex64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -556,15 +418,9 @@ func (s *GLMstack) PushComplex64(x complex64) error {
 
 func (s *GLMstack) TsPushComplex64(x complex64) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Complex64size)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Complex64size)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                           //入栈记录
+	sl := s.tspushsafetycheck(Complex64size) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*complex64)(sp)
 	*sp2 = x
@@ -574,9 +430,7 @@ func (s *GLMstack) TsPushComplex64(x complex64) error {
 }
 
 func (s *GLMstack) PushInterface(x interface{}) error {
-	if s.size+Interfacesize >= s.scap {
-		s.scap = s.addcap(s.size + Interfacesize)
-	}
+	s.pushsafetycheck(Interfacesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
 	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
@@ -588,15 +442,9 @@ func (s *GLMstack) PushInterface(x interface{}) error {
 
 func (s *GLMstack) TsPushInterface(x interface{}) error {
 	s.mutex.RLock()
-	s.pushrecord() //入栈记录
-	nsize := atomic.AddUint64(&s.size, Interfacesize)
-	if nsize >= s.scap {
-		s.mutex.RUnlock()
-		s.scap = s.Tsaddcap(s.size + Interfacesize)
-		s.mutex.RLock()
-	}
+	s.pushrecord()                           //入栈记录
+	sl := s.tspushsafetycheck(Interfacesize) //入栈安全检查
 	sp := unsafe.Pointer(&(s.slice[0]))
-	sl := s.size
 	sp = unsafe.Pointer(uintptr(sp) + uintptr(sl))
 	sp2 := (*interface{})(sp)
 	*sp2 = x
