f887bd3188e809559d1aff755d2c0d33b2e085fb
秋来冬风 <2645477756@qq.com>
2022-02-04 21:58:05 +0800

    GLMstack

--- stack/GLMstack.go
+++ stack/GLMstack.go
@@ -134,31 +134,6 @@ func (s *GLMstack) Tsclear() error {
 	return nil
 }
 
-func (s *GLMstack) Popptr(ptr unsafe.Pointer, size uint64) error {
-	sizei := s.size
-	s.size -= size
-	vptr := uintptr(unsafe.Pointer(&s.slice[0])) + uintptr(sizei)
-	uptr := uintptr(ptr)
-	for i := uint64(0); i < size; i++ {
-		*(*int8)(unsafe.Pointer(vptr + (uintptr(i)))) = *(*int8)(unsafe.Pointer(uptr + uintptr(i)))
-	}
-	return nil
-}
-
-func (s *GLMstack) TsPopptr(ptr unsafe.Pointer, size uint64) error {
-	s.mutex.RLock()
-	s.poprecord() //出栈记录
-	sizei := atomic.AddUint64(&s.size, ^(size - 1))
-	vptr := uintptr(unsafe.Pointer(&s.slice[0])) + uintptr(sizei)
-	uptr := uintptr(ptr)
-	for i := uint64(0); i < size; i++ { //实际出栈
-		*(*int8)(unsafe.Pointer(vptr + (uintptr(i)))) = *(*int8)(unsafe.Pointer(uptr + uintptr(i)))
-	}
-	s.popok() //结束记录
-	s.mutex.RUnlock()
-	return nil
-}
-
 func (s *GLMstack) TsLoadpp() int64 {
 	return atomic.LoadInt64(&s.pp)
 }
