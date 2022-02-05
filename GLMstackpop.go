4b5835d3b5d8142e348439d4f6e0916cdc7b8a7e
秋来冬风 <2645477756@qq.com>
2022-02-05 18:24:09 +0800

    GLMstack

---
+++ GLMstackpop.go
@@ -0,0 +1,35 @@
+// GLMstackpop
+package stack
+
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
+func (s *GLMstack) Popptr(ptr unsafe.Pointer, size uint64) error {
+	safe := popsafetycheck(size) //出栈安全检查
+	if safe != safeOk {
+		return StackContentShortage
+	}
+	s.size -= size
+	vptr := uintptr(unsafe.Pointer(&s.slice[0])) + uintptr(s.size)
+	uptr := uintptr(ptr)
+	for i := uint64(0); i < size; i++ {
+		*(*int8)(unsafe.Pointer(uptr + uintptr(i))) = *(*int8)(unsafe.Pointer(vptr + (uintptr(i))))
+	}
+	return nil
+}
+
+func (s *GLMstack) TsPopptr(ptr unsafe.Pointer, size uint64) error {
+	s.mutex.RLock()
+	s.poprecord() //出栈记录
+	sizei := atomic.AddUint64(&s.size, ^(size - 1))
+	vptr := uintptr(unsafe.Pointer(&s.slice[0])) + uintptr(sizei)
+	uptr := uintptr(ptr)
+	for i := uint64(0); i < size; i++ { //实际出栈
+		*(*int8)(unsafe.Pointer(uptr + uintptr(i))) = *(*int8)(unsafe.Pointer(vptr + (uintptr(i))))
+	}
+	s.popok() //结束记录
+	s.mutex.RUnlock()
+	return nil
+}
