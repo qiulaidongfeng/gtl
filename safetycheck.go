e17f61f23f732f6cadb1dd5d67168adacf3b5b69
秋来冬风 <2645477756@qq.com>
2022-02-05 18:24:48 +0800

    GLMstack

---
+++ safetycheck.go
@@ -0,0 +1,35 @@
+// safetycheck
+package stack
+
+import (
+	"sync/atomic"
+)
+
+const (
+	safeOk int8 = iota
+	notEnoughSpace
+	shortageStackContent
+)
+
+func (s *GLMstack) pushsafetycheck(size uint64) (safe int8) {
+	if s.size+size >= s.scap {
+		safe = notEnoughSpace
+	}
+	return
+}
+
+func (s *GLMstack) tspushsafetycheck(size uint64) (sizeold uint64, safe int8, nsize uint64) {
+	nsize = atomic.AddUint64(&s.size, size)
+	if nsize >= s.scap {
+		safe = notEnoughSpace
+	}
+	sizeold = nsize - size
+	return
+}
+
+func (s *GLMstack) popsafetycheck(size uint64) (safe int8) {
+	if size > s.size {
+		safe = shortageStackContent
+	}
+	return
+}
