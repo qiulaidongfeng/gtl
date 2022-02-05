fd1dbfa173c0c4129486137e67a04f64b2e7e250
秋来冬风 <2645477756@qq.com>
2022-02-05 22:15:59 +0800

    GLMstack

--- GLMstack.go
+++ GLMstack.go
@@ -147,6 +147,10 @@ func (s *GLMstack) TsLoadpushn() int64 {
 }
 
 func (s *GLMstack) Addcap(ncap uint64) (err error) {
+	safe := addcapsafetycheck(ncap) //新容量安全检查
+	if safe != safeOk {
+		return StackNcapSmall
+	}
 	nslice := make([]int8, ncap, ncap)
 	copy(nslice, s.slice)
 	s.slice = nslice
@@ -155,6 +159,10 @@ func (s *GLMstack) Addcap(ncap uint64) (err error) {
 
 func (s *GLMstack) TsAddcap(ncap uint64) (err error) {
 	s.mutex.Lock()
+	safe := addcapsafetycheck(ncap) //新容量安全检查
+	if safe != safeOk {
+		return StackNcapSmall
+	}
 	nslice := make([]int8, ncap, ncap)
 	copy(nslice, s.slice)
 	s.slice = nslice
@@ -163,5 +171,8 @@ func (s *GLMstack) TsAddcap(ncap uint64) (err error) {
 }
 
 func (s *GLMstack) Subcap(ncap uint64) (err error) {
-
+	nslice := make([]int8, ncap, ncap)
+	copy(nslice, s.slice)
+	s.slice = nslice
+	return nil
 }
