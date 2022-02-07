0b6927333739ef4b77d2f89933a214628b7ac167
秋来冬风 <2645477756@qq.com>
2022-02-07 19:49:28 +0800

    errors

--- errors/wrap.go
+++ errors/wrap.go
@@ -1,6 +1,7 @@
-// warp
+// wrap
 package errors
 
+//被包装的错误类型
 type WrapError struct {
 	err     error
 	message string
@@ -10,14 +11,17 @@ func (err WrapError) Error() string {
 	return err.message + err.err.Error()
 }
 
+//返回被包装的错误
 func (err WrapError) Unwrap() error {
 	return err.err
 }
 
+//包装错误
 func WithMessage(err error, message string) error {
 	return WrapError{err: err, message: message}
 }
 
-func WrapError(err error, message string) error {
+//包装错误
+func NewWrapError(err error, message string) error {
 	return WrapError{err: err, message: message}
 }
