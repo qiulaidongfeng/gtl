76f757c633797251257aab671ed76fd4e81c6720
秋来冬风 <2645477756@qq.com>
2022-04-24 17:37:50 +0800

    虚拟内存操作开发完毕

---
+++ sys/virtual-memory_uinx.go
@@ -0,0 +1,67 @@
+//go:build !windows
+// +build !windows
+
+package sys
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+//指定虚拟内存操作的结构体
+type Virtual_memory struct {
+	addr   uintptr
+	length uintptr
+}
+
+//指定虚拟内存操作的结构体别名
+type VM = Virtual_memory
+
+//创建指定虚拟内存操作的结构体
+func NewVM(addr, length uintptr) VM {
+	return VM{
+		addr:   addr,
+		length: length,
+	}
+}
+
+func mtob(addr, length uintptr) []byte {
+	var a []byte
+	p := (*[3]uintptr)(unsafe.Pointer(&a))
+	p[0] = addr
+	p[1] = length
+	p[2] = length
+	return a
+}
+
+//锁定指定虚拟内存操作，有错panic
+func (v VM) Lock() {
+	pslice := mtob(v.addr, v.length)
+	err := syscall.Mlock(pslice)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//锁定指定虚拟内存操作，有错返回error
+func (v VM) Lockerr() error {
+	pslice := mtob(v.addr, v.length)
+	err := syscall.Mlock(pslice)
+	return err
+}
+
+//解锁指定虚拟内存操作，有错panic
+func (v VM) Unlock() {
+	pslice := mtob(v.addr, v.length)
+	err := syscall.Munlock(pslice)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//解锁指定虚拟内存操作，有错error
+func (v VM) Unlockerr() error {
+	pslice := mtob(v.addr, v.length)
+	err := syscall.Munlock(pslice)
+	return err
+}
---
+++ sys/virtual-memory_windows.go
@@ -0,0 +1,50 @@
+package sys
+
+import (
+	"syscall"
+)
+
+//指定虚拟内存操作的结构体
+type Virtual_memory struct {
+	addr   uintptr
+	length uintptr
+}
+
+//指定虚拟内存操作的结构体别名
+type VM = Virtual_memory
+
+//创建指定虚拟内存操作的结构体
+func NewVM(addr, length uintptr) VM {
+	return VM{
+		addr:   addr,
+		length: length,
+	}
+}
+
+//锁定指定虚拟内存操作，有错panic
+func (v VM) Lock() {
+	err := syscall.VirtualLock(v.addr, v.length)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//锁定指定虚拟内存操作，有错返回error
+func (v VM) Lockerr() error {
+	err := syscall.VirtualLock(v.addr, v.length)
+	return err
+}
+
+//解锁指定虚拟内存操作，有错panic
+func (v VM) Unlock() {
+	err := syscall.VirtualUnlock(v.addr, v.length)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//解锁指定虚拟内存操作，有错error
+func (v VM) Unlockerr() error {
+	err := syscall.VirtualUnlock(v.addr, v.length)
+	return err
+}
